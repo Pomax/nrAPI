82b36249b6c74278f9847b2c9318779f17fb20e7
Pomax <pomax@nihongoresources.com>
2014-06-06 16:04:26 -0700

    cleanup

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+module.exports = function(dataHandler) {
+	return {
+    index: function(req, res) {
+      res.render("index.html");
+    },
+    entry: function(req, res) {
+      dataHandler.findEntry(req.id, function(err, result) {
+        if(err) {
+          return res.json({ error: err });
+        }
+        res.json(result);
+      });
+    },
+    find: function(req, res, next) {
+      dataHandler.findAll(req.term, function(err, results) {
+        if(err) {
+          return res.json({ error: err });
+        }
+        res.json(results);
+      });
+    },
+    show: function(req, res) {
+      dataHandler.findAll(req.term, function(err, results) {
+        if(err) {
+          return res.json({ error: err });
+        }
+        res.render("result.html", { results: results });
+      });
+    }
+  };
+};
---
+++ routes/parameters.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+  return function() {
+  	var params = Array.prototype.slice.call(arguments);
+	  params.forEach(function(param) {
+	    app.param(param, function(req, res, next, value) {
+	      req[param] = value;
+	      next();
+	    });
+	  });
+	};
+};
+
+parameters("id", "term");
