36db6380006d55a63da74b6406f64b4119e63fb6
Pomax <pomax@nihongoresources.com>
2014-06-26 21:20:46 +0200

    .

--- routes/parameters.js
+++ routes/parameters.js
@@ -3,6 +3,13 @@ module.exports = function(app) {
   params.forEach(function(param) {
     app.param(param, function(req, res, next, value) {
       req[param] = value;
+      if(param === "dict") {
+        // CORS is based on the :dict parameter
+        res.header("Access-Control-Allow-Origin", [
+          "http://nihongoresources.com",
+          "http://api.nihongoresources.com"
+        ].join(' '));
+      }
       next();
     });
   });
