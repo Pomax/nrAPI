cdb83d62e5f2ccb728ea5a33a5e7e117a48a0083
Pomax <pomax@nihongoresources.com>
2014-06-27 11:25:11 +0200

    crash prevention

--- routes/parameters.js
+++ routes/parameters.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
       req[param] = value;
       if(param === "dict") {
         var origin = req.get('origin');
-        if(origin.match(/([^.]+\.)?nihongoresources\.com/)) {
+        if(origin && origin.match(/([^.]+\.)?nihongoresources\.com/)) {
           res.header("Access-Control-Allow-Origin", origin);
         }
       }
